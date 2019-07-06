015ba67cbf07d05d844eac9e53b8eb468564a005
abhishek <abhishek.sen999@gmail.com>
2019-07-06 23:44:00 +0530

    added web scrapping

--- extension/linkdownloader_video.js
+++ extension/linkdownloader_video.js
@@ -3,9 +3,23 @@ function download()
     
     let archive_url=document.getElementById("archive_link").value;
     //archive_url=archive_url.toString().trim();
+    downloadLink(archive_url);
     console.log("started- "+archive_url);
-    document.getElementById('my_iframe').src=archive_url;
+    
+    console.log("started- "+archive_url);
 }
-console.log("here");
-//document.getElementById("download").onclick()     
+function downloadLink(url)
+{
+    document.getElementById('my_iframe').src=url;
+}
 
+
+
+
+console.log("here");
+// document.getElementById("download").onclick="download()";     
+$(document).ready(function(){
+    $("#download").click(function(){
+        download();
+    });
+  });
